e0c6b8f35a0f3784d10afacf6137a3ae2f3f2f1b
chipsmelly <128431826+chipsmelly@users.noreply.github.com>
2024-11-12 14:19:43 -0600

    1.5
    Reworked the way the card element grid uses the filters to determine which cards should show up.  Uses a refreshGrid function every time a button is clicked that evaluates the filters in a more efficient way, looking at cards one at a time to decide if they should be displayed or not.

--- index.js
+++ index.js
@@ -8,95 +8,94 @@ let international = false;
 let categoryFilter = false;
 let locationFilter = false;
 
+function refreshGrid( ) {
 
+    for (i = 0; document.querySelectorAll(".grid .cell-outer").length; i++) {
+        if(categoryFilter === true) {
+            if (legal === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("legal") === true) {
+                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+            } else if (research === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("research") === true) {
+                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+            } else if (industry === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("industry") === true) {
+                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+            } else if (events === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("events") === true) {
+                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+            } else {
+                document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+                continue;
+            }
+    
+            if (locationFilter === true) {
+                if (us === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("us") === false) {
+                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+                    
+                } else if (international === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === false) {
+                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+                }
+            }
+        }
+        else {
+            document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+
+            if (locationFilter === true) {
+                if (us === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("us") === false) {
+                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+                    
+                } else if (international === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === false) {
+                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+                }
+            }
+        }    
+    }
+}
 
 document.querySelector("#legal-button").addEventListener("click", function () {
     if (legal === false) {
 
-        if (categoryFilter === false) {
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-            }
-            categoryFilter = true;
-        }
-
-        for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-            if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("legal") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-        }
-
         legal = true;
         document.querySelector("#legal-button").style.backgroundColor = "limegreen";
 
-    } else {
-
-        if (research === true || industry === true || events === true) {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("legal") === true) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                }
-            }
-
-        } else {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-
-            categoryFilter = false;
-
+        if (categoryFilter === false) {
+            categoryFilter = true;
         }
+
+    } else {
         
         legal = false;
         document.querySelector("#legal-button").style.backgroundColor = "white";
+
+        if (research === false && events === false && industry === false) {
+            categoryFilter = false;
+        }
+
     }
+
+    refreshGrid();
     
 } );
 
 document.querySelector("#research-button").addEventListener("click", function () {
     if (research === false) {
 
-        if (categoryFilter === false) {
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-            }
-            categoryFilter = true;
-        }
-
-        for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-            if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("research") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-        }
-
         research = true;
         document.querySelector("#research-button").style.backgroundColor = "limegreen";
 
+        if (categoryFilter === false) {
+            categoryFilter = true;
+        }
+
     } else {
 
-        if (legal === true || industry === true || events === true) {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("research") === true) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                }
-            }
-
-        } else {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-            
-            categoryFilter = false;
-
-        }
-        
         research = false;
         document.querySelector("#research-button").style.backgroundColor = "white";
+
+        if (legal === false && events === false && industry === false) {
+            categoryFilter = false;
+        }
+
     }
+
+    refreshGrid();
     
 } );
 
@@ -104,90 +103,50 @@ document.querySelector("#research-button").addEventListener("click", function ()
 document.querySelector("#industry-button").addEventListener("click", function () {
     if (industry === false) {
 
-        if (categoryFilter === false) {
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-            }
-            categoryFilter = true;
-        }
-
-        for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-            if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("industry") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-        }
-
         industry = true;
         document.querySelector("#industry-button").style.backgroundColor = "limegreen";
 
+        if (categoryFilter === false) {
+            categoryFilter = true;
+        }
+
     } else {
 
-        if (legal === true || research === true || events === true) {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("industry") === true) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                }
-            }
-
-        } else {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-            
-            categoryFilter = false;
-
-        }
-        
         industry = false;
         document.querySelector("#industry-button").style.backgroundColor = "white";
+
+        if (research === false && events === false && industry === false) {
+            categoryFilter = false;
+        }
+
     }
+
+    refreshGrid();
     
 } );
 
 document.querySelector("#events-button").addEventListener("click", function () {
     if (events === false) {
 
-        if (categoryFilter === false) {
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-            }
-            categoryFilter = true;
-        }
-
-        for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-            if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("events") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-        }
-
         events = true;
         document.querySelector("#events-button").style.backgroundColor = "limegreen";
 
-    } else {
-
-        if (legal === true || research === true || industry === true) {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("events") === true) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                }
-            }
-
-        } else {
-
-            for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            }
-            
-            categoryFilter = false;
-
+        if (categoryFilter === false) {
+            categoryFilter = true;
         }
         
+    } else {
+
         events = false;
         document.querySelector("#events-button").style.backgroundColor = "white";
+
+        if (research === false && events === false && industry === false) {
+            categoryFilter = false;
+        }
+
     }
+
+    refreshGrid();
     
 } );
 
@@ -196,64 +155,55 @@ document.querySelector("#events-button").addEventListener("click", function () {
 document.querySelector("#us-button").addEventListener("click", function () {
     if (us === false) {
 
-        if (international === true) {
-
-
-
-            international = false;
-            document.querySelector("#international-button").style.backgroundColor = "white";
-        }
-
-        // if (categoryFilter === true) {
-
-        //     for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-
-        //         if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === true) {
-        //             document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-        //         }
-
-        //     }
-
-        // } else {
-
-
-
-        // }
-
         us = true;
         document.querySelector("#us-button").style.backgroundColor = "limegreen";
 
+        if (international === true) {
+
+            international = false;
+            document.querySelector("#international-button").style.backgroundColor = "white";
+
+        } else {
+
+            locationFilter = true;
+
+        }
+
     } else {
 
-        
-        
         us = false;
         document.querySelector("#us-button").style.backgroundColor = "white";
+        locationFilter = false;
     }
+
+    refreshGrid();
+
 } );
 
 document.querySelector("#international-button").addEventListener("click", function () {
     if (international === false) {
 
+        international = true;
+        document.querySelector("#international-button").style.backgroundColor = "limegreen";
+        
         if (us === true) {
-
-
 
             us = false;
             document.querySelector("#us-button").style.backgroundColor = "white";
+        } else {
+
+            locationFilter = true;
+
         }
-
-        
-
-        international = true;
-        document.querySelector("#international-button").style.backgroundColor = "limegreen";
 
     } else {
 
-        
-        
         international = false;
         document.querySelector("#international-button").style.backgroundColor = "white";
+        locationFilter = false;
     }
+
+    refreshGrid();
+
 } );
 
