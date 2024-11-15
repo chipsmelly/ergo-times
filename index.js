b5dd418d9f3fee591e2870cfd5abaee99eb2e1fb
chipsmelly <128431826+chipsmelly@users.noreply.github.com>
2024-11-15 13:05:50 -0600

    2.0
    Moved article data to JSON file (articles.json) rather than having data hard coded into HTML.
    Implemented search function (upon input, so every time a key is entered or removed).
    Reworked filter buttons so set visibility based on data found in JSON.
    
    Need to make the search bar and buttons work together..

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// Nav Bar Filters
+
+// Variables used
+
+
+
+const template = document.querySelector("[grid-template]")
+const cardContainer = document.querySelector("[card-container]")
+const searchInput = document.querySelector("[search]")
+
+
+let articles = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    articles.forEach(article => {
+        const isVisible = article.title.toLowerCase().includes(value)
+        article.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+
+fetch("./articles.json")
+.then(res => res.json())
+.then(data => {
+    articles = data.map(article => {
+        const card = template.content.cloneNode(true).children[0]
+        const logo = card.querySelector("[cell-logo]")
+        const categoryTag = card.querySelector("[category-tag]")
+        const locationTag = card.querySelector("[location-tag]")
+        const title = card.querySelector("[cell-title]")
+
+        logo.src = article.image
+        categoryTag.textContent = article.category
+        const catClass = article.category.toLowerCase()
+        categoryTag.classList.add(catClass)
+        locationTag.textContent = article.location 
+        const locClass = article.location.toLowerCase()
+        locationTag.classList.add(locClass)
+        title.textContent = article.title 
+
+        cardContainer.append(card)
+        return { image: article.image, category: article.category, location: article.location, title: article.title, element: card }
+    })
+})
+
 let legal = false;
 let research = false;
 let industry = false;
@@ -8,45 +54,86 @@ let international = false;
 let categoryFilter = false;
 let locationFilter = false;
 
-function refreshGrid( ) {
+// function refreshGrid( ) {
 
-    for (i = 0; document.querySelectorAll(".grid .cell-outer").length; i++) {
-        if(categoryFilter === true) {
-            if (legal === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("legal") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            } else if (research === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("research") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            } else if (industry === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("industry") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            } else if (events === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("events") === true) {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
-            } else {
-                document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                continue;
-            }
+//     for (i = 0; document.querySelectorAll(".grid .cell-outer").length; i++) {
+//         if(categoryFilter === true) {
+//             if (legal === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("legal") === true) {
+//                 document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+//             } else if (research === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("research") === true) {
+//                 document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+//             } else if (industry === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("industry") === true) {
+//                 document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+//             } else if (events === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("events") === true) {
+//                 document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+//             } else {
+//                 document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+//                 continue;
+//             }
     
-            if (locationFilter === true) {
-                if (us === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("us") === false) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+//             if (locationFilter === true) {
+//                 if (us === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("us") === false) {
+//                     document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
                     
-                } else if (international === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === false) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+//                 } else if (international === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === false) {
+//                     document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+//                 }
+//             }
+//         }
+//         else {
+//             document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
+
+//             if (locationFilter === true) {
+//                 if (us === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("us") === false) {
+//                     document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+                    
+//                 } else if (international === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === false) {
+//                     document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
+//                 }
+//             }
+//         }    
+//     }
+// }
+
+function refreshGrid() {
+
+    articles.forEach(article => {
+        let isVisible = false;
+
+        if (categoryFilter === true) {
+            if (legal === true && article.category.toLowerCase().includes("legal")) {
+                isVisible = true
+            } else if (research === true && article.category.toLowerCase().includes("research")) {
+                isVisible = true
+            } else if (industry === true && article.category.toLowerCase().includes("industry")) {
+                isVisible = true
+            } else if (events === true && article.category.toLowerCase().includes("events")) {
+                isVisible = true
+            }
+
+            if (locationFilter === true) {
+                if (us === true && !article.location.toLowerCase().includes("us")) {
+                    isVisible = false
+                } else if (international === true && !article.location.toLowerCase().includes("international")) {
+                    isVisible = false
+                }
+            }
+        } else {
+            isVisible = true;
+
+            if (locationFilter === true) {
+                if (us === true && !article.location.toLowerCase().includes("us")) {
+                    isVisible = false
+                } else if (international === true && !article.location.toLowerCase().includes("international")) {
+                    isVisible = false
                 }
             }
         }
-        else {
-            document.querySelectorAll(".grid .cell-outer")[i].style.display = "flex";
 
-            if (locationFilter === true) {
-                if (us === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("us") === false) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                    
-                } else if (international === true && document.querySelectorAll(".grid .cell-outer")[i].classList.contains("international") === false) {
-                    document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-                }
-            }
-        }    
-    }
+        article.element.classList.toggle("hide", !isVisible)
+
+    })
+
 }
 
 document.querySelector("#legal-button").addEventListener("click", function () {
@@ -71,6 +158,9 @@ document.querySelector("#legal-button").addEventListener("click", function () {
     }
 
     refreshGrid();
+
+    console.log(legal)
+    console.log(categoryFilter)
     
 } );
 
@@ -206,4 +296,3 @@ document.querySelector("#international-button").addEventListener("click", functi
     refreshGrid();
 
 } );
-
